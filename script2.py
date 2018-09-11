3415ccc2242c060f518557d86f58133f97ec04c0
JPFC1991 <jpfonsec@hotmail.com>
2018-09-11 18:07:43 -0500

    my naive commit

--- script2.py
+++ script2.py
@@ -1,4 +1,5 @@
 
 for i in range(120):
     if i == 10:
-        print("Perfect 10!")
+        print("Awesome 9!")
+a="additional string"
--- script3.py
+++ script3.py
@@ -1,1 +1,4 @@
-print("I'm just here for the beer")
+print("I'm just here for the chicks")
+wxy = "September 11, 2018"
+print("Date?")
+print(wxy) # only works Sept. 11, 2018
